72e739ff0f06529669a8b610cdecda7ecc8b424f
alice <122629939+espcaa@users.noreply.github.com>
2025-10-10 00:12:45 +0200

    added pause menu

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -5,10 +5,27 @@ func _input(event: InputEvent) -> void:
 	# catch the "pause" action to toggle the pause menu
 	if Input.is_action_just_pressed("pause") and not event.echo:
 		if UserData.paused == false:
-			$AnimationPlayer.play("pause")
-			await $AnimationPlayer.animation_finished
-			UserData.paused = true
+			pause_game()
 		else:
-			$AnimationPlayer.play_backwards("pause")
-			await $AnimationPlayer.animation_finished
-			UserData.paused = false
+			await unpause_game()
+
+
+func _on_quit_button_pressed() -> void:
+	Transitioner.load_scene_with_transition(
+		"res://scenes/main_menu.tscn", self, Enums.ColorRole.SECONDARY
+	)
+
+
+func _on_resume_button_pressed() -> void:
+	await unpause_game()
+
+
+func unpause_game() -> void:
+	$AnimationPlayer.play_backwards("pause")
+	await $AnimationPlayer.animation_finished
+	UserData.paused = false
+
+
+func pause_game() -> void:
+	$AnimationPlayer.play("pause")
+	UserData.paused = true
