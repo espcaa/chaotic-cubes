f82e42cfcdc0402e54d171b7e649f68c0f81aaaa
alice <122629939+espcaa@users.noreply.github.com>
2025-10-28 10:52:08 +0100

    mirror dice logic fixed

--- scenes/dice_mirror.gd
+++ scenes/dice_mirror.gd
@@ -10,30 +10,13 @@ var dice_lore = "is it a trick? or just magic?"
 func roll():
 	# get the last played value
 	
-
+	var last_value = get_tree().get_first_node_in_group("gameui").get_recent_dice_value()
 	if playing:
 		return  # prevent rolling multiple times
 
-	playing = true
-	var duration = 1.0
-	var step = 0.15
-	var elapsed = 0.0
-	var last_face = -1
-
-	while elapsed < duration:
-		# Ensure a different face is shown
-		var new_face = faces[randi() % faces.size()]
-		while new_face == last_face and faces.size() > 1:
-			new_face = faces[randi() % faces.size()]
-		$AnimatedSprite2D.frame = new_face
-		play_woosh()
-		await get_tree().create_timer(step).timeout
-		elapsed += step
-
-	# Stop rolling
-	playing = false
-	$CustomLabel.label_text = str($AnimatedSprite2D.frame + 1) + " !!"
-	value.append($AnimatedSprite2D.frame + 1)  # +1 because frames are 0-based
+	$CustomLabel.label_text = str(last_value) + " mirorr!!"
+	value.append(last_value)
+	await get_tree().process_frame
 	emit_signal("roll_finished")
 	$AnimationPlayer.play("text")
 	
